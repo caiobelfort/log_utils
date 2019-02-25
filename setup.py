baa90a7d1be85a492916be05172ab9f90edb71fe
Caio Belfort <caiobelfort90@gmail.com>
2019-02-25 18:22:36 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='log_utils',
+    version='0.1a',
+    description='Utilities for logging in Python',
+    author='Caio Belfort',
+    author_email='caiobelfort90@gmail.com',
+    license='GPL',
+    py_modules=['parallel_queries'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Database',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.7',
+        'Operation System :: POSIX'
+    ]
+)
